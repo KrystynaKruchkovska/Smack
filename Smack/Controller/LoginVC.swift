a3df3ba6c3af1dc9d3fd8e409d4831985837eba5
Krystyna Kruchkovska <kruchok883@gmail.com>
2018-08-22 11:10:57 +0300

    Login VC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Mac on 8/22/18.
+//  Copyright © 2018 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    @IBAction func closePressed(_ sender: Any) {
+       dismiss(animated: true,completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Mac on 8/22/18.
+//  Copyright © 2018 Jonny B. All rights reserved.
+//
+
+import Foundation
+// segues
+let TO_LOGIN = "toLogin"
